0384ea8336a0062badfecd560352b3bd03a7a19f
guikoblitz <guilhermerk_contato@hotmail.com>
2022-03-26 17:59:08 -0300

    refactor: main page composition api refactor

--- src/screens/MainPage.ts
+++ src/screens/MainPage.ts
@@ -1,17 +1,63 @@
-import { getDistrictsByState, getStates } from 'src/services/IbgeService';
-import { defineComponent } from 'vue';
+import { Loading, QTable } from 'quasar';
+import lodash from 'lodash';
+import { onMounted, reactive, watch } from 'vue';
+import { State } from 'src/common/interfaces/State';
+import { getStatesUseCase } from 'src/useCases/StateUseCase';
+import { District } from 'src/common/interfaces/District';
+import { getDistrictsByStateUseCase } from 'src/useCases/DistrictUseCase';
 
-export default defineComponent({
-  name: 'MainPage',
-  data() {
-    return {};
-  },
-  async mounted(): Promise<void> {
-    console.log('TESTE');
-    const teste = await getStates();
-    console.log(teste);
+export default {
+  setup() {
+    const state = reactive({
+      selectedState: null as State | null,
+      stateOptions: [] as State[],
+      stateData: [] as District[],
+      teste: '',
+    });
+    const districtsTableColumns = [
+      {
+        name: 'name',
+        align: 'left',
+        label: 'NOME',
+        headerStyle: 'min-width: 140px',
+        required: true,
+      },
+      {
+        name: 'ibgeCode',
+        align: 'left',
+        label: 'CÓDIGO IBGE',
+        headerStyle: 'min-width: 140px',
+        required: true,
+      },
+    ] as QTable['columns'];
 
-    const teste2 = await getDistrictsByState(41);
-    console.log(teste2);
+    onMounted(async () => {
+      state.stateOptions = await getStatesUseCase();
+      console.log(state.stateOptions);
+    });
+
+    watch(
+      () => state.selectedState,
+      (val) => {
+        console.log(val);
+      }
+    );
+
+    async function selectState(): Promise<void> {
+      if (state.selectedState) {
+        Loading.show({ message: 'Carregando Distritos...' });
+        const stateDistricts = await getDistrictsByStateUseCase(state.selectedState.id);
+        if (stateDistricts) state.stateData = lodash.cloneDeep(stateDistricts);
+
+        console.log(state.stateData);
+        Loading.hide();
+      }
+    }
+
+    function selectDistrict(district: District): void {
+      console.log(district);
+    }
+
+    return { state, districtsTableColumns, selectState, selectDistrict };
   },
-});
+};
--- src/screens/MainPage.vue
+++ src/screens/MainPage.vue
@@ -1,5 +1,43 @@
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <q-page class="q-py-lg side-spacing">
+    <div>
+      <q-select
+        ref="stateSelector"
+        label="UF"
+        v-model="state.selectedState"
+        :options="state.stateOptions"
+        outlined
+        option-value="id"
+        option-label="label"
+        dense
+        transition-show="flip-up"
+        transition-hide="flip-down"
+        @update:model-value="selectState()"
+      />
+    </div>
+    <div class="q-pt-sm" v-if="state.stateData && state.stateData.length > 0">
+      <q-table ref="districtsTable" title="Dados IBGE" :columns="districtsTableColumns" :rows="state.stateData" row-key="name" dense>
+        <template v-slot:body="props">
+          <q-tr :props="props" @click="selectDistrict(props.row)" style="font-weight: 500; cursor: pointer">
+            <q-td key="name" :props="props">
+              {{ props.row.name }}
+            </q-td>
+            <q-td key="ibgeCode" :props="props">
+              {{ props.row.id }}
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+    </div>
+    <div class="q-pt-sm" v-else>Não tem tabela</div>
+  </q-page>
 </template>
 
 <script src="./MainPage.ts" />
+
+<style>
+.side-spacing {
+  padding-left: 150px;
+  padding-right: 150px;
+}
+</style>
