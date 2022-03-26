d256878c84554c03b6b01267e2d5610e9edd3bf8
guikoblitz <guilhermerk_contato@hotmail.com>
2022-03-26 17:58:53 -0300

    refactor: service refactor

--- src/services/IbgeService.ts
+++ src/services/IbgeService.ts
@@ -1,7 +1,8 @@
+import { DistrictDTO } from 'src/common/dtos/DistrictDTO';
+import { StateDTO } from 'src/common/dtos/StateDTO';
 import ibgeApi from './Api';
 
-async function getStates(): Promise<any | undefined> {
-  // TODO tipar o any
+async function getStates(): Promise<StateDTO[] | null> {
   try {
     const response = await ibgeApi.get('estados');
     if (response.status === 200) {
@@ -14,8 +15,20 @@ async function getStates(): Promise<any | undefined> {
   }
 }
 
-async function getDistrictsByState(uf: number | string): Promise<any | undefined> {
-  // TODO tipar o any
+async function getState(uf: number | string): Promise<StateDTO | null> {
+  try {
+    const response = await ibgeApi.get(`estados/${uf}`);
+    if (response.status === 200) {
+      return response.data;
+    }
+    return null;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+async function getDistrictsByState(uf: number | string): Promise<DistrictDTO[] | null> {
   try {
     const response = await ibgeApi.get(`estados/${uf}/distritos`);
     if (response.status === 200) {
@@ -28,4 +41,4 @@ async function getDistrictsByState(uf: number | string): Promise<any | undefined
   }
 }
 
-export { getStates, getDistrictsByState };
+export { getStates, getState, getDistrictsByState };
